ab4645463437dcf9dec25e72f25669a2c083bf59
joever.monceda <joever.monceda@gmail.com>
2024-03-13 15:01:40 +0800

    setup initial project

---
+++ Models/LoginCredentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EDCS.API.Integration.Models
+{
+    public class LoginCredentials
+    {
+        [JsonPropertyName("userName")]
+        public string UsernName { set; get; }
+        [JsonPropertyName("password")]
+        public string Password { set; get; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EDCS.API.Integration.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("EDCS API Integration.");
+
+string contentType = "application/json";
+
+string edcsUrl = "https://test02-edcs-web-appsvc.azurewebsites.net";
+
+var userLogin = new LoginCredentials
+{
+    UsernName = "superadmin",
+    Password = "Reversed123"
+};
+
+using (HttpClient client = new HttpClient())
+{
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType));
+
+    StringContent userLoginContent = new StringContent(JsonSerializer.Serialize(userLogin), Encoding.UTF8, contentType);
+
+    Console.Write("User login request..");
+
+    var loginRequest = await client.PostAsync($"{edcsUrl}/login", userLoginContent);
+
+    if (loginRequest.StatusCode == System.Net.HttpStatusCode.OK)
+    {
+        var loginResponseContent = JsonSerializer.Deserialize<LoginResponse>(await loginRequest.Content.ReadAsStringAsync());
+
+        Console.WriteLine("Uploading PR Payload..");
+
+        string prPayload = File.ReadAllText("PRPayload.json");
+
+        StringContent prPayloadContent = new StringContent(prPayload, Encoding.UTF8, contentType);
+
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {loginResponseContent!.AccessToken}");
+
+        HttpResponseMessage prUploadRequest = await client.PostAsync($"{edcsUrl}/promotional-reports/load-pr", prPayloadContent);
+
+        string prUploadResponse = await prUploadRequest.Content.ReadAsStringAsync();
+
+        Console.Write($"PR upload response status: {prUploadResponse}");
+    }
+    else
+    {
+        Console.WriteLine("Unable to login user..");
+    }
+}
